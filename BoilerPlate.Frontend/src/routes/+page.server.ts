775229d6b1ab8e7925049a0227f6962ecb2f2c41
Rui Sousa <rsousa@codeliturgy.com>
2024-08-25 21:38:06 +0100

    Docker file for frontend

--- BoilerPlate.Frontend/src/routes/+page.server.ts
+++ BoilerPlate.Frontend/src/routes/+page.server.ts
@@ -1,10 +1,11 @@
 import type { PageServerLoad } from './$types';
+import { PUBLIC_BASE_URL } from '$env/static/public'
 
 type OutputType = { users: [] }
 
 export const load: PageServerLoad<OutputType> = async ({ fetch, params, request }) => {
     
-    const response = await fetch("/api/users");
+    const response = await fetch(PUBLIC_BASE_URL + "/api/users");
     const responseData = await response.json();
 
     return {
